eb13491c17d49b9f6718b7faefc57b055b551629
desc0n <desconbk@yandex.ru>
2016-12-17 19:28:59 +1000

    Переношу готовый контроллер crm

--- application/classes/Controller/Crm.php
+++ application/classes/Controller/Crm.php
@@ -4,6 +4,10 @@ class Controller_Crm extends Controller_Base
 {
     public function getBaseTemplate()
     {
+        if (!Auth::instance()->logged_in('admin')) {
+            HTTP::redirect('/crm/login');
+        }
+
         return View::factory('crm/template')
             ->set('get', $_GET)
             ->set('post', $_POST)
@@ -21,11 +25,6 @@ class Controller_Crm extends Controller_Base
             HTTP::redirect('/');
         }
 
-        /**
-         * @var $adminModel Model_Admin
-         */
-        $adminModel = Model::factory('Admin');
-
         $template = $this->getBaseTemplate();
 
         $template->content = View::factory('crm/index')
@@ -38,7 +37,7 @@ class Controller_Crm extends Controller_Base
     {
         if (!Auth::instance()->logged_in() && isset($_POST['login'])) {
             Auth::instance()->login($this->request->post('username'), $this->request->post('password'),true);
-            HTTP::redirect('/crm');
+            HTTP::redirect('/crm/notices_list');
         }
 
         $template = View::factory('crm/login')
@@ -48,6 +47,15 @@ class Controller_Crm extends Controller_Base
         $this->response->body($template);
     }
 
+    public function action_logout()
+    {
+        if (Auth::instance()->logged_in() && isset($_POST['logout'])) {
+            Auth::instance()->logout();
+
+            HTTP::redirect('/');
+        }
+    }
+
     public function action_registration()
     {
         $template = View::factory('crm/registration')
@@ -55,46 +63,51 @@ class Controller_Crm extends Controller_Base
             ->set('error', '')
         ;
 
-        if (empty(Arr::get($_POST,'username'))) {
-            $template->set('error', '<div class="alert alert-danger"><strong>Не указан логин!</strong> Укажите Ваш логин.</div>');
-        } elseif (empty(Arr::get($_POST,'email'))) {
-            $template->set('error', '<div class="alert alert-danger"><strong>Не указана почта!</strong> Укажите Вашу почту.</div>');
-        } elseif (Arr::get($_POST,'password','')=="") {
-            $template->set('error', '<div class="alert alert-danger"><strong>Не указан пароль!</strong> Укажите Ваш пароль.</div>');
-        } else if (Arr::get($_POST,'password') != Arr::get($_POST,'password2')) {
-            $template->set('error', '<div class="alert alert-danger"><strong>Пароли не совпадают!</strong> Проверьте правильность подтверждения пароля.</div>');
-        } else {
-            $user = ORM::factory('User');
-            $user->values(array(
-                'username' => $_POST['username'],
-                'email' => $_POST['email'],
-                'password' => $_POST['password'],
-                'password_confirm' => $_POST['password2'],
-            ));
-            $some_error = false;
+        if (count($this->request->post())) {
+            if (empty(Arr::get($_POST,'username'))) {
+                $template->set('error', '<div class="alert alert-danger"><strong>Не указан логин!</strong> Укажите Ваш логин.</div>');
+            } elseif (empty(Arr::get($_POST,'email'))) {
+                $template->set('error', '<div class="alert alert-danger"><strong>Не указана почта!</strong> Укажите Вашу почту.</div>');
+            } elseif (Arr::get($_POST,'password','')=="") {
+                $template->set('error', '<div class="alert alert-danger"><strong>Не указан пароль!</strong> Укажите Ваш пароль.</div>');
+            } else if (Arr::get($_POST,'password') != Arr::get($_POST,'password2')) {
+                $template->set('error', '<div class="alert alert-danger"><strong>Пароли не совпадают!</strong> Проверьте правильность подтверждения пароля.</div>');
+            } else {
+                $user = ORM::factory('User');
+                $user->values(array(
+                    'username' => $_POST['username'],
+                    'email' => $_POST['email'],
+                    'password' => $_POST['password'],
+                    'password_confirm' => $_POST['password2'],
+                ));
+                $some_error = false;
 
-            try {
-                $user->save();
-                $user->add("roles",ORM::factory("Role",1));
-            }
-            catch (ORM_Validation_Exception $e) {
-                $some_error = $e->errors('models');
-            }
-            if ($some_error) {
-                $template->set('error', '<div class="alert alert-danger"><strong>Ошибка регистрационных данных!</strong> Проверьте правильность ввода данных.</div>');
-
-                if (isset($some_error['username'])) {
-                    if ($some_error['username'] == "models/user.username.unique") {
-                        $template->set('error', '<div class="alert alert-danger"><strong>Такой логин уже есть в базе!</strong> Придумайте новый логин.</div>');
-                    }
+                try {
+                    $user->save();
+                    $user->add("roles",ORM::factory("Role",1));
                 }
-                else if (isset($some_error['email'])) {
-                    if ($some_error['email']=="email address must be an email address") {
-                        $template->set('error', '<div class="alert alert-danger"><strong>Некорректный формат почты!</strong> Проверьте правильность написания почты.</div>');
+                catch (ORM_Validation_Exception $e) {
+                    $some_error = $e->errors('models');
+                }
+
+                if ($some_error) {
+                    $template->set('error', '<div class="alert alert-danger"><strong>Ошибка регистрационных данных!</strong> Проверьте правильность ввода данных.</div>');
+
+                    if (isset($some_error['username'])) {
+                        if ($some_error['username'] == "models/user.username.unique") {
+                            $template->set('error', '<div class="alert alert-danger"><strong>Такой логин уже есть в базе!</strong> Придумайте новый логин.</div>');
+                        }
                     }
-                    if ($some_error['email']=="models/user.email.unique") {
-                        $template->set('error', '<div class="alert alert-danger"><strong>Такая почта есть в базе!</strong> Укажите другую почту.</div>');
+                    else if (isset($some_error['email'])) {
+                        if ($some_error['email']=="email address must be an email address") {
+                            $template->set('error', '<div class="alert alert-danger"><strong>Некорректный формат почты!</strong> Проверьте правильность написания почты.</div>');
+                        }
+                        if ($some_error['email']=="models/user.email.unique") {
+                            $template->set('error', '<div class="alert alert-danger"><strong>Такая почта есть в базе!</strong> Укажите другую почту.</div>');
+                        }
                     }
+                } else {
+                    HTTP::redirect('/crm');
                 }
             }
         }
@@ -102,63 +115,120 @@ class Controller_Crm extends Controller_Base
         $this->response->body($template);
     }
 
-    public function action_sale()
+    public function action_add_notice()
     {
-        /** @var $actionModel Model_Action */
-        $actionModel = Model::factory('Action');
+        /** @var $noticeModel Model_Notice */
+        $noticeModel = Model::factory('Notice');
 
-        $saleId = $this->request->param('id');
-
-        if ($this->request->post('redactActionClient') !== null) {
-            $actionModel->setActionCustomer(
-                $this->request->post('redactActionClient'),
-                $this->request->post('name'),
-                $this->request->post('address'),
-                $this->request->post('tk'),
-                $this->request->post('phone'),
-                $this->request->post('email')
-            );
-
-            HTTP::redirect($this->request->referrer());
-        }
-
-        if ($this->request->post('addDelivery') !== null) {
-            $actionModel->addSaleDelivery($saleId);
-
-            HTTP::redirect($this->request->referrer());
-        }
-
-        $saleData = $actionModel->getSaleData($saleId);
+        /** @var $contentModel Model_Content */
+        $contentModel = Model::factory('Content');
 
         $template = $this->getBaseTemplate();
 
-        $template->content = View::factory('crm/sale_info')
-            ->set('saleData', $saleData)
-            ->set('saleProducts', $actionModel->getSaleProductsData($saleId))
-            ->set('get', $this->request->query())
+        $template->content = View::factory('crm/add_notice')
+            ->set('districts', $contentModel->findAllDistricts())
+            ->set('types', $noticeModel->findAllTypes())
+        ;
+
+        if (isset($_POST['addnotice'])) {
+            $id = $noticeModel->addNotice($_POST);
+            HTTP::redirect('/crm/redact_notice/' . $id);
+        }
+
+        $this->response->body($template);
+    }
+
+    public function action_redact_notice()
+    {
+        /** @var $noticeModel Model_Notice */
+        $noticeModel = Model::factory('Notice');
+
+        /** @var $contentModel Model_Content */
+        $contentModel = Model::factory('Content');
+
+        $template = $this->getBaseTemplate();
+
+        $template->content = View::factory('crm/redact_notice')
+            ->set('districts', $contentModel->findAllDistricts())
+            ->set('noticeData', $noticeModel->findById($this->request->param('id')))
+            ->set('types', $noticeModel->findAllTypes())
+        ;
+
+        $filename=Arr::get($_FILES, 'imgname', []);
+        if (!empty($filename)) {
+            $noticeModel->loadNoticeImg($_FILES, $this->request->param('id'));
+            HTTP::redirect($this->request->referrer());
+        }
+
+        if (isset($_POST['redact_notice'])) {
+            $noticeModel->setNotice($_POST);
+
+            HTTP::redirect($this->request->referrer());
+        }
+
+        $this->response->body($template);
+    }
+
+    public function action_notices_list()
+    {
+        /** @var $noticeModel Model_Notice */
+        $noticeModel = Model::factory('Notice');
+
+        $template = $this->getBaseTemplate();
+
+        $template->content = View::factory('crm/notices_list')
+            ->set('noticesList', $noticeModel->getList(
+                Arr::get($this->request->query(), 'page', 1),
+                Arr::get($this->request->query(), 'limit', 20)
+            ))
+            ->set('paginationCount', $noticeModel->getListPaginationCount(Arr::get($this->request->query(), 'limit', 20)))
+            ->set('page', Arr::get($this->request->query(), 'page', 1))
+            ->set('types', $noticeModel->findAllTypes())
         ;
 
         $this->response->body($template);
     }
 
-    public function action_sales_list()
+    public function action_pages_list()
     {
-        /** @var $actionModel Model_Action */
-        $actionModel = Model::factory('Action');
+        /** @var $adminModel Model_Admin */
+        $adminModel = Model::factory('Admin');
+
+        /** @var $contentModel Model_Content */
+        $contentModel = Model::factory('Content');
 
         $template = $this->getBaseTemplate();
 
-        $startDate = DateTime::createFromFormat('d.m.Y', null != $this->request->query('start') ? $this->request->query('start') : date('d.m.Y'));
-        $endDate = DateTime::createFromFormat('d.m.Y', null != $this->request->query('end') ? $this->request->query('end') : date('d.m.Y'));
+        if (isset($_POST['redactpage'])) {
+            $adminModel->setPage($_POST);
+            HTTP::redirect($this->request->referrer());
+        }
 
-        $start = null != $this->request->query('start') ? $startDate->format('d.m.Y') : $startDate->modify('- 1 week')->format('d.m.Y');
-        $end = $endDate->format('d.m.Y');
+        $template->content = View::factory('crm/pages_list')
+            ->set('pages', $contentModel->getPages())
+        ;
 
-        $template->content = View::factory('crm/sales_list')
-            ->set('actionsList', $actionModel->findAllSales($start, $end))
-            ->set('get', $this->request->query())
-            ->set('start', $start)
-            ->set('end', $end)
+        $this->response->body($template);
+    }
+
+    public function action_redact_page()
+    {
+        /** @var $adminModel Model_Admin */
+        $adminModel = Model::factory('Admin');
+
+        /** @var $contentModel Model_Content */
+        $contentModel = Model::factory('Content');
+
+        $template = $this->getBaseTemplate();
+
+        if (isset($_POST['redact_page'])) {
+            $adminModel->setPage($this->request->query('id'), $this->request->post('content'));
+
+            HTTP::redirect($this->request->referrer());
+        }
+
+        $template->content = View::factory('crm/redact_page')
+            ->set('pageData', $contentModel->findPageById((int)$this->request->query('id')))
         ;
 
         $this->response->body($template);
